fb06cf8d5ed006ae5df9d2b7406768d860d9c902
Shimul Das <shimul.191002110@gmail.com>
2023-03-27 12:05:30 +0600

    Update 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 const number=5555;
-const singer={
-  name:"mafuz Rahman",
-  Job: 'singer'
-}
+// const singer={
+//   name:"mafuz Rahman",
+//   Job: 'singer'
+// }
+const singers=[
+  {name:"mafuz Rahman",Job: 'singer'},
+  {name:"mafuz Rahman",Job: 'singer'},
+  {name:"mafuz Rahman",Job: 'singer'},
+  {name:"mafuz Rahman",Job: 'singer'}
+]
 const SingerStyle={
   color:'green',
   backgroundColor:'yellow'
 }
-function App() {
+function App(){
+const nayioks = ['shakib khan','bapparaz','josim','rubel','riyaz','razzak'];
+
   return (
+    
     <div className="App">
-      <Person name="Shimul Chandra Das" nayika="Porna Paul"></Person>
-      <Person name="Shakibal Hassan" nayika="Sumiya Tabassum"></Person>
+      {
+        nayioks.map(nayiok => <li>Name: {nayiok}</li>)
+      }
+      {
+        nayioks.map(nayiok => <Person name={nayiok}></Person>)
+      }
+      {/* <Person name="Shimul Chandra Das" nayika="Porna Paul"></Person>
+      <Person name="Shakibal Hassan" nayika="Sumiya Tabassum"></Person> */}
+      {/* <Person name={nayioks[0]} nayika="Porna Paul"></Person>
+      <Person name={nayioks[1]} nayika="Sumiya Tabassum"></Person>  */}
 
     </div>
   );
 }
+// function Person(props){
+//   // return <h1>Banglar King Shakibal Hassan</h1>
+//   console.log(props)
+//   return(
+//     <div className='person'>
+//     <h1>Name: {props.name}</h1>
+//     <h1>Nayika: {props.nayika}</h1>
+//     <p>His Profession is cricketer</p>
+//     </div>
+
+//   )
+// }
+
 function Person(props){
   // return <h1>Banglar King Shakibal Hassan</h1>
+  
   console.log(props)
   return(
     <div className='person'>
